98c776fdacddb266ab6d532de1a8f15c08465bfe
Jasmine <jasminegit config --global user.email jasmine_singh1992@hotmail.comgit config --global user.name Your>
2020-02-12 00:11:42 +0000

    pair partner Bobby Uno

--- timer1.js
+++ timer1.js
@@ -1,12 +1,10 @@
 const beeps =process.argv.slice(2);
 let time = 0;
 for (let i = 0; i < beeps.length; i++ ) {
-  time += beeps[i] * 1000;
-setTimeout(() => {
-  process.stdout.write('\x07');
-}, time);
+  time = beeps[i] * 1000;
+  if(beeps[i] > 0  && beeps[i] !== NaN){
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, time);
+  }
 }
-
-
-
-
